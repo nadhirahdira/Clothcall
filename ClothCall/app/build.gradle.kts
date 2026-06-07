d68842c03ff20fe18baf25e0952d78f3aecbb583
Amen <amenwoldesenbet7@gmail.com>
2026-06-07 16:35:25 +0900

    Update build.gradle.kts

--- ClothCall/app/build.gradle.kts
+++ ClothCall/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 2
-        versionName = "2.0"
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
